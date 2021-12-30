8b2c490d4c73aa5fa1c430d2515ce9bfd4a648c9
alberto <alberto@LAPTOP-7R9E1PIM>
2021-12-30 13:17:32 +0000

    Kata 5P1 - Version 1

---
+++ src/kata5p1/Kata5P1.java
@@ -0,0 +1,38 @@
+package kata5p1;
+
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Kata5P1 {
+
+    public static void main(String[] args) throws FileNotFoundException, FileNotFoundException {
+        String selectAllSQL = "SELECT * FROM PEOPLE";
+        try ( Connection conn = connect();  
+                Statement stmt = conn.createStatement();  
+                ResultSet rs = stmt.executeQuery(selectAllSQL)) {
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t"
+                        + rs.getString("Nombre") + "\t"
+                        + rs.getString("Apellidos") + "\t"
+                        + rs.getString("Departamento") + "\t");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static Connection connect() {
+        String url = "jdbc:sqlite:KATA5.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+}
